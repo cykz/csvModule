091905f6ef17fb2270972be7e6060fa029d5530f
cykz <cykz@cykz.com>
2020-05-16 23:32:24 +0200

    add write module

--- csvModule.py
+++ csvModule.py
@@ -6,3 +6,7 @@ with open ('query.csv' , newline='') as csvfile:
   for row in queryreader:
     print(', '.join(row))
 
+with open ('query.csv', 'w', newline='') as csvfile:
+  querywriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+  querywriter.writerrow(['Eleonora Debor'] * 5 + ['John Miller'])
+  querywriter.writerrow(['Brad Poed', 'Stefan Drox'])
